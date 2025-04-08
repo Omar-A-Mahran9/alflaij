202f79a9a7a63870f719627798f69eae5e5073ec
hany-sam <hany.samir@webstdy.com>
2025-04-08 14:03:05 +0200

    update resource

--- app/Http/Resources/CarResource.php
+++ app/Http/Resources/CarResource.php
@@ -29,8 +29,7 @@ class CarResource extends JsonResource
         'price_field_status'=>__($price_field_status) === __('others') ? $this->other_description:__($price_field_status),
         'price'=>$price_field_status === PriceFieldStatus::show_details->name  && $this->have_discount?number_format($this->discount_price):0,
         'price_before_discount'=>$price_field_status === PriceFieldStatus::show_details->name?number_format($this->price) :0,
-        'price_after_vat' =>$price_field_status === PriceFieldStatus::show_details->name ? ($this->price_after_vat ==$this->price ? 0 :number_format($this->price_after_vat) ):0,
-        'price_after_tax' => $price_field_status === PriceFieldStatus::show_details->name 
+         'price_after_vat' => $price_field_status === PriceFieldStatus::show_details->name 
         ? (settings()->getSettings('maintenance_mode') == 1
             ? round($this->price * (1 + $tax / 100)) // Price with VAT for maintenance mode 1
             : null) // Price with VAT for maintenance mode 0 (as per your requirement)
