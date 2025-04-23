006f72fd385f4a3bfdad7f58c5760dacabc5cc5a
hany-sam <hany.samir@webstdy.com>
2025-04-23 10:11:28 +0200

    update

--- resources/views/pay/payment.blade.php
+++ resources/views/pay/payment.blade.php
@@ -21,7 +21,7 @@
             publishable_api_key: 'pk_test_BEUSsbAZsH5r8CajSvqta9gDwEM62tfn4YPweTQs',
 
             // Callback to your Laravel server
-            callback_url: "http://127.0.0.1:8000/dashboard/pay",
+            callback_url: "http://127.0.0.1:8000/api/pay",
 
             methods: ['creditcard']
         });
